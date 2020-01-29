dbdf708acb5a38ea02533e5fb99606b8227b40e9
Hermes Zhang <zhangchenhui2006@gmail.com>
2020-01-29 15:24:46 +0800

    Support topN contribution

--- gitv.py
+++ gitv.py
@@ -3,7 +3,8 @@ import git
 import pyecharts.options as opts
 from pyecharts.charts import Bar, Timeline
 
-repo = git.Repo(r'~/django-bootstrap-toolkit', odbt=git.GitCmdObjectDB)
+repo = git.Repo(r'~/work/reveal.js', odbt=git.GitCmdObjectDB)
+#repo = git.Repo(r'~/django-bootstrap-toolkit', odbt=git.GitCmdObjectDB)
 commits = pd.DataFrame(repo.iter_commits('master'), columns=['raw'])
 
 commits['author'] = commits['raw'].apply(lambda x: x.author.name)
@@ -15,7 +16,7 @@ commits['delete'] = commits['raw'].apply(lambda x: x.stats.total['deletions'])
 
 new = commits.iloc[::-1]
 
-def get_commits_chart(time: str, names: list, c_insert: list, c_delete: list) -> Bar:
+def get_commits_chart(time: str, names: list, c_insert: list, c_delete: list, c_add: list) -> Bar:
     bar = (
         Bar()
         .add_xaxis(xaxis_data=names)
@@ -28,6 +29,12 @@ def get_commits_chart(time: str, names: list, c_insert: list, c_delete: list) ->
         .add_yaxis(
             series_name="Delete",
             yaxis_data=c_delete,
+            is_selected=True,
+            label_opts=opts.LabelOpts(is_show=True),
+        )
+        .add_yaxis(
+            series_name="Add",
+            yaxis_data=c_add,
             is_selected=True,
             label_opts=opts.LabelOpts(is_show=True),
         )
@@ -47,6 +54,7 @@ authors = []
 contributes = []
 insert_lines = []
 delete_lines = []
+top = {}
 
 for row in new.itertuples():
     _, _, author, date, lines, insert, delete = row
@@ -54,17 +62,29 @@ for row in new.itertuples():
     if author not in authors:
         authors.append(author)
         contributes.append(lines)
-        insert_lines.append(insert)
-        delete_lines.append(delete)
+        top[author] = (insert - delete, insert, delete)
     else:
         contributes[authors.index(author)] += lines
-        insert_lines[authors.index(author)] += insert
-        delete_lines[authors.index(author)] += delete
+        top[author] = (top[author][0] + insert - delete, top[author][1] + insert, top[author][2] + delete)
 
-    timeline.add(get_commits_chart(time=date,
-        names=authors.copy(),
-        c_insert=insert_lines.copy(),
-        c_delete=delete_lines.copy()),
+    top10 = sorted(top.items(), reverse=True, key=lambda x: x[1][0])[:5]
+
+    c_authors = []
+    insert_lines = []
+    delete_lines = []
+    add_lines = []
+
+    for elem in top10:
+        c_authors.append(elem[0])
+        add_lines.append(elem[1][0])
+        insert_lines.append(elem[1][1])
+        delete_lines.append(elem[1][2])
+
+    timeline.add(get_commits_chart(time=date, \
+        names=c_authors, \
+        c_insert=insert_lines, \
+        c_delete=delete_lines, \
+        c_add=add_lines), \
         time_point=date)
 
 # 1.0.0 版本的 add_schema 暂时没有补上 return self 所以只能这么写着
