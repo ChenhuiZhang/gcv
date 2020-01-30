77e82b1bc03359e77211c50bae91dd233575d0b1
Hermes Zhang <zhangchenhui2006@gmail.com>
2020-01-30 14:22:39 +0800

    code refactoring

--- gitv.py
+++ gitv.py
@@ -1,23 +1,32 @@
+""" TODO """
+
 import pandas as pd
 import git
 import pyecharts.options as opts
 from pyecharts.charts import Bar, Timeline
 
-repo = git.Repo(r'~/work/reveal.js', odbt=git.GitCmdObjectDB)
-#repo = git.Repo(r'~/django-bootstrap-toolkit', odbt=git.GitCmdObjectDB)
-commits = pd.DataFrame(repo.iter_commits('master'), columns=['raw'])
+def get_repo():
+    """
+    TODO
+    """
+    #repo = git.Repo(r'~/work/reveal.js', odbt=git.GitCmdObjectDB)
+    repo = git.Repo(r'~/django-bootstrap-toolkit', odbt=git.GitCmdObjectDB)
+    commits = pd.DataFrame(repo.iter_commits('master'), columns=['raw'])
 
-commits['author'] = commits['raw'].apply(lambda x: x.author.name)
-commits['committed_date'] = commits['raw'].apply(
-    lambda x: x.committed_datetime.strftime('%Y-%m-%d %H:%M:%S'))
-commits['lines'] = commits['raw'].apply(lambda x: x.stats.total['lines'])
-commits['insert'] = commits['raw'].apply(lambda x: x.stats.total['insertions'])
-commits['delete'] = commits['raw'].apply(lambda x: x.stats.total['deletions'])
+    commits['author'] = commits['raw'].apply(lambda x: x.author.name)
+    commits['committed_date'] = commits['raw'].apply(\
+            lambda x: x.committed_datetime.strftime('%Y-%m-%d %H:%M:%S'))
+    commits['lines'] = commits['raw'].apply(lambda x: x.stats.total['lines'])
+    commits['insert'] = commits['raw'].apply(lambda x: x.stats.total['insertions'])
+    commits['delete'] = commits['raw'].apply(lambda x: x.stats.total['deletions'])
 
-new = commits.iloc[::-1]
+    return commits.iloc[::-1]
 
 def get_commits_chart(time: str, names: list, c_insert: list, c_delete: list, c_add: list) -> Bar:
-    bar = (
+    """
+    TODO
+    """
+    contribute = (
         Bar()
         .add_xaxis(xaxis_data=names)
         .add_yaxis(
@@ -44,49 +53,56 @@ def get_commits_chart(time: str, names: list, c_insert: list, c_delete: list, c_
             ),
         )
     )
-    return bar
+    return contribute
 
+def get_topX(top, row, x):
+    """
+    TODO
+    """
+    _, _, author, _, _, insert, delete = row
 
-# 生成时间轴的图
-timeline = Timeline(init_opts=opts.InitOpts(width="1600px", height="800px"))
+    contribute = pd.DataFrame(columns=['author', 'add', 'insert', 'delete'])
 
-authors = []
-contributes = []
-insert_lines = []
-delete_lines = []
-top = {}
-
-for row in new.itertuples():
-    _, _, author, date, lines, insert, delete = row
-
-    if author not in authors:
-        authors.append(author)
-        contributes.append(lines)
+    if author not in top:
         top[author] = (insert - delete, insert, delete)
     else:
-        contributes[authors.index(author)] += lines
-        top[author] = (top[author][0] + insert - delete, top[author][1] + insert, top[author][2] + delete)
+        top[author] = (top[author][0] + insert - delete, \
+                top[author][1] + insert, \
+                top[author][2] + delete)
 
-    top10 = sorted(top.items(), reverse=True, key=lambda x: x[1][0])[:5]
+    topX = sorted(top.items(), reverse=True, key=lambda x: x[1][0])[:x]
 
-    c_authors = []
-    insert_lines = []
-    delete_lines = []
-    add_lines = []
+    for elem in topX:
+        rec = pd.Series(\
+                {'author': elem[0], 'add': elem[1][0], 'insert': elem[1][1], 'delete': elem[1][2]})
+        contribute = contribute.append(rec, ignore_index=True)
 
-    for elem in top10:
-        c_authors.append(elem[0])
-        add_lines.append(elem[1][0])
-        insert_lines.append(elem[1][1])
-        delete_lines.append(elem[1][2])
+    return contribute
 
-    timeline.add(get_commits_chart(time=date, \
-        names=c_authors, \
-        c_insert=insert_lines, \
-        c_delete=delete_lines, \
-        c_add=add_lines), \
-        time_point=date)
+def gcv_main():
+    """
+    TODO
+    """
+    # 生成时间轴的图
+    timeline = Timeline(init_opts=opts.InitOpts(width="1600px", height="800px"))
 
-# 1.0.0 版本的 add_schema 暂时没有补上 return self 所以只能这么写着
-timeline.add_schema(is_auto_play=True, play_interval=100)
-timeline.render("git.html")
+    commit_hist = get_repo()
+
+    contribute = {}
+
+    for item in commit_hist.itertuples():
+        top_commiter = get_topX(contribute, item, 5)
+
+        timeline.add(get_commits_chart(time=item[3], \
+                names=top_commiter['author'].tolist(), \
+                c_insert=top_commiter['insert'].tolist(), \
+                c_delete=top_commiter['delete'].tolist(), \
+                c_add=top_commiter['add'].tolist()), \
+                time_point=item[3])
+
+    # 1.0.0 版本的 add_schema 暂时没有补上 return self 所以只能这么写着
+    timeline.add_schema(is_auto_play=True, play_interval=100)
+    timeline.render("git.html")
+
+if __name__ == "__main__":
+    gcv_main()
